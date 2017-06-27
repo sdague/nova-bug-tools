6ed7a7c9170c835d16f3e42c7eac0f1b4e582140
Sean Dague <sean@dague.net>
2017-06-27 15:20:07 -0400

    refactor fix in progress bugs

--- openstack_bugs/messages.py
+++ openstack_bugs/messages.py
@@ -10,8 +10,14 @@ NO_STACK_VERS_FOUND = (
     "required, marking as Incomplete. Please update the "
     "bug description to include '%s version: ... '.")
 
-NO_REVIEWS = (
+NOT_INPROGRESS = (
     "This bug is not In Progress, and has no open patches in the "
     "comments, so it is being unassigned. Please do not take ownership of "
     "bugs if you have a patch to submit for them to ensure that people are "
     "not discouraged from looking at these bugs.")
+
+NO_REVIEWS = (
+    "There are no currently open reviews on this bug, changing "
+    "the status back to the previous state and unassigning. If "
+    "there are active reviews related to this bug, please include "
+    "links in comments. ")
